1be6f47533586c1706073e79ec8a9f8944d90e7f
Felipe <castanheirafc2@gmail.com>
2023-08-13 14:58:17 -0300

    remove summary

--- src/components/SelectWithSearch.js
+++ src/components/SelectWithSearch.js
@@ -1,35 +1,40 @@
 const WRAPPER_ID = 'options_wrapper';
-const SUMMARY_TEXT = 'Select Product';
 const SEARCH_TEXT = 'Search';
 
 class SelectWithSearch {
   constructor (element, handleClick) {
     this.element = element;
-    this.summary = document.createElement('summary');
-    this.summary.innerText = SUMMARY_TEXT;
-    this.element.appendChild(this.summary);
     this.wrapper = document.createElement('div');
     this.handleClick = handleClick;
+    this.textList = [];
   }
 
   addList (textList) {
-    this.textList = textList || [];
+    this.textList = textList.map((item) => {
+      const upperText = item.text.toUpperCase();
+      return {...item, upperText }
+    });
   }
 
-  appendOption (text) {
+  appendOption (item) {
     const li = document.createElement('li');
-    li.addEventListener('click', () => this.handleClick(text));
-    li.innerText = text;
+    const { upperText, ...itemProps } = item;
+    li.addEventListener('click', () => {
+      this.handleClick(itemProps);
+      this.reset();
+    });
+    li.innerText = item.text;
     this.ul.appendChild(li);
   }
 
   showOptions ({ value }) {
     this.ul.innerHTML = '';
     if (value.length < 2) return;
+    const upperCaseValue = value.toUpperCase();
     for (let i = 0; i < this.textList.length; i++) {
-      const text = this.textList[i];
-      if (text.toUpperCase().includes(value.toUpperCase())){
-        this.appendOption(text);
+      const item = this.textList[i];
+      if (item.upperText.includes(upperCaseValue)){
+        this.appendOption(item);
       }
     }
   }
@@ -44,12 +49,18 @@ class SelectWithSearch {
     const input = document.createElement('input');
     input.placeholder = SEARCH_TEXT;
     input.addEventListener('keyup', (e) => this.showOptions(e.target));
+    this.input = input;
     this.wrapper.appendChild(input);
   }
 
   createUL () {
     this.ul = document.createElement('ul');
     this.wrapper.appendChild(this.ul);
+  }
+
+  reset () {
+    this.input.value = '';
+    this.ul.innerHTML = '';
   }
 
   config () {
